8ec7cd6eca0e93e381148929addd424d9e8c2ba0
yash <yashkumar201301@gmail.com>
2021-03-20 18:00:09 +0530

    dynamic broadcast receiver

--- app/src/main/java/com/example/threads/java/BroadcastReceiverActivity.java
+++ app/src/main/java/com/example/threads/java/BroadcastReceiverActivity.java
@@ -2,6 +2,9 @@ package com.example.threads.java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,16 +12,24 @@ import com.example.threads.R;
 
 public class BroadcastReceiverActivity extends AppCompatActivity {
 
+    MyBroadcastReceiver broadcastReceiver = new MyBroadcastReceiver();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_broadcast_reciever);
+
+        registerReceiver(broadcastReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
     }
 
     public void sendBroadcast(View view) {
 
-
-
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(broadcastReceiver);
+    }
 }
--- app/src/main/java/com/example/threads/java/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/threads/java/MyBroadcastReceiver.java
@@ -3,21 +3,34 @@ package com.example.threads.java;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.widget.Toast;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            Toast.makeText(context, "BOOT", Toast.LENGTH_SHORT).show();
-        } else if (Intent.ACTION_AIRPLANE_MODE_CHANGED.equals(intent.getAction())) {
-            Toast.makeText(context, "AIRPLANE MODE", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(context, "CONNECTIVITY CHANGED", Toast.LENGTH_SHORT).show();
-        }
-    }
 
+//        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+//            Toast.makeText(context, "BOOT", Toast.LENGTH_SHORT).show();
+//        } else if (Intent.ACTION_AIRPLANE_MODE_CHANGED.equals(intent.getAction())) {
+//            Toast.makeText(context, "AIRPLANE MODE", Toast.LENGTH_SHORT).show();
+//        } else {
+//            Toast.makeText(context, "CONNECTIVITY CHANGED", Toast.LENGTH_SHORT).show();
+//        }
+
+        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
+            boolean isChange = intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+            if (!isChange) {
+                Toast.makeText(context, "CONNECTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "DISCONNECTED", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+
+    }
 
 
 }
